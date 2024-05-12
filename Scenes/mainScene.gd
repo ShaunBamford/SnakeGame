3d031f73c21aa9234f11b4e810746ff78d5ff9bd
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-05-12 02:49:44 +0100

    Game upload

--- Scenes/mainScene.gd
+++ Scenes/mainScene.gd
@@ -1,16 +1,147 @@
-extends Node2D
+extends Node
 
+@export var snake_scene : PackedScene
+
+#game variables
 var score : int
-var gameStarted : bool = false
+var game_started : bool = false
 
+#grid variables
 var cells : int = 20
-var cellSize : int = 50
+var cell_size : int = 50
 
-# Called when the node enters the scene tree for the first time.
+#food variables
+var food_pos : Vector2
+var regen_food : bool = true
+
+#snake variables
+var old_data : Array
+var snake_data : Array
+var snake : Array
+
+#movement variables
+var start_pos = Vector2(9, 9)
+var up = Vector2(0, -1)
+var down = Vector2(0, 1)
+var left = Vector2(-1, 0)
+var right = Vector2(1, 0)
+var move_direction : Vector2
+var can_move: bool
+
+# Called when game starts
 func _ready():
-	pass # Replace with function body.
-
-
+	new_game()
+	
+func new_game():
+	get_tree().paused = false
+	get_tree().call_group("segments", "queue_free")
+	$GameOverMenu.hide()
+	score = 0
+	$Hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
+	move_direction = up
+	can_move = true
+	generate_snake()
+	move_food()
+	
+# generates snake
+func generate_snake():
+	old_data.clear()
+	snake_data.clear()
+	snake.clear()
+	#starting with the start_pos, create tail segments vertically down
+	for i in range(3):
+		add_segment(start_pos + Vector2(0, i))
+		
+func add_segment(pos):
+	snake_data.append(pos)
+	var SnakeSegment = snake_scene.instantiate()
+	SnakeSegment.position = (pos * cell_size) + Vector2(0, cell_size)
+	add_child(SnakeSegment)
+	snake.append(SnakeSegment)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move_snake()
+	
+func move_snake():
+	if can_move:
+		#update movement from keypresses
+		if Input.is_action_just_pressed("ui_down") and move_direction != up:
+			move_direction = down
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("ui_up") and move_direction != down:
+			move_direction = up
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("ui_left") and move_direction != right:
+			move_direction = left
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("ui_right") and move_direction != left:
+			move_direction = right
+			can_move = false
+			if not game_started:
+				start_game()
+
+func start_game():
+	game_started = true
+	$MoveTimer.start()
+
+
+func _on_move_timer_timeout():
+	#allow snake movement
+	can_move = true
+	#use the snake's previous position to move the segments
+	old_data = [] + snake_data
+	snake_data[0] += move_direction
+	for i in range(len(snake_data)):
+		#move all the segments along by one
+		if i > 0:
+			snake_data[i] = old_data[i - 1]
+		snake[i].position = (snake_data[i] * cell_size) + Vector2(0, cell_size)
+	check_out_of_bounds()
+	check_self_eaten()
+	check_food_eaten()
+	
+	# check if snake has gone out of bounds, if so end game.
+func check_out_of_bounds():
+	if snake_data[0].x < 0 or snake_data[0].x > cells - 1 or snake_data[0].y < 0 or snake_data[0].y > cells - 1:
+		end_game()
+	# check if snake has ate snake, if so end game.		
+func check_self_eaten():
+	for i in range(1, len(snake_data)):
+		if snake_data[0] == snake_data[i]:
+			end_game()
+			
+func check_food_eaten():
+	#if snake eats the food, add a segment and move the food
+	if snake_data[0] == food_pos:
+		score += 1
+		$Hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
+		add_segment(old_data[-1])
+		move_food()
+
+# moves food when food been ate
+func move_food():
+	while regen_food:
+		regen_food = false
+		food_pos = Vector2(randi_range(0, cells - 1), randi_range(0, cells - 1))
+		for i in snake_data:
+			if food_pos == i:
+				regen_food = true
+	$Food.position = (food_pos * cell_size)+ Vector2(0, cell_size)
+	regen_food = true
+# ends game and displays a game over screen
+func end_game():
+	$GameOverMenu.show()
+	$MoveTimer.stop()
+	game_started = false
+	get_tree().paused = true
+
+# when play again pressed, restart game
+func _on_game_over_menu_restart():
+	new_game()
